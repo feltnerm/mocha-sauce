9d6445f0bc99f9ec3acdd52410cf2e277c26b3bb
Mark Feltner <feltner.mj@gmail.com>
2013-07-31 12:53:26 -0500

    feature(index.js): Run selenium tests
    Allows user to run arbitrary selenium code in between page load and test execution finish.

--- index.js
+++ index.js
@@ -7,171 +7,181 @@
  * Copyright 2013 Paul Bakaus, licensed under MIT
  * "mocha-cloud" is Copyright 2013 TJ Holowaychuk
  */
-
 var wd = require('wd'),
-	Emitter = require('events').EventEmitter,
-	debug = require('debug')('mocha-sauce'),
-	Batch = require('batch'),
-	request = require('request');
+    Emitter = require('events').EventEmitter,
+    debug = require('debug')('mocha-sauce'),
+    Batch = require('batch'),
+    request = require('request');
 
 function MochaSauce(conf) {
 
-	this.name = conf.name;
-	this.user = conf.username || process.env.SAUCE_USER_NAME;
-	this.key = conf.accessKey || process.env.SAUCE_API_KEY;
-	this.host = conf.host || process.env.SELENIUM_HOST || "ondemand.saucelabs.com";
-	this.port = conf.port || process.env.SELENIUM_PORT || 80;
+    this.name = conf.name;
+    this.user = conf.username || process.env.SAUCE_USER_NAME;
+    this.key = conf.accessKey || process.env.SAUCE_API_KEY;
+    this.host = conf.host || process.env.SELENIUM_HOST || "ondemand.saucelabs.com";
+    this.port = conf.port || process.env.SELENIUM_PORT || 80;
 
-	this.browsers = [];
+    this.browsers = [];
 
-    this._urls = conf.urls || [];
-	//this._url = conf.url || '';
-	this._concurrency = 2;
-	this.tags = conf.tags || [];
-	this.build = conf.build || '';
-	this._video = false;
-	this._screenshots = false;
+    this._url = conf.url || '';
+    this._concurrency = conf.concurrency || 2;
+    this.tags = conf.tags || [];
+    this.build = conf.build || '';
+    this._video = false;
+    this._screenshots = false;
 
 }
 
 MochaSauce.prototype.__proto__ = Emitter.prototype;
 
-MochaSauce.prototype.build = function(build) {
-	this.build = build;
-	return this;
-};
-
-MochaSauce.prototype.tags = function(tags) {
-	this.tags = tags;
-	return this;
-};
-
-MochaSauce.prototype.url = function(url) {
-    this._urls.push(url);
-	return this;
-};
-
-MochaSauce.prototype.urls = function(urls) {
-    this._urls = url;
+MochaSauce.prototype.build = function (build) {
+    this.build = build;
     return this;
-}
-
-MochaSauce.prototype.concurrency = function(num) {
-	this._concurrency = num;
-	return this;
 };
 
-MochaSauce.prototype.record = function(video, screenshots) {
-
-	if(screenshots === undefined) {
-		screenshots = video;
-	}
-
-	this._video = video;
-	this._screenshots = screenshots;
-
-	return this;
+MochaSauce.prototype.tags = function (tags) {
+    this.tags = tags;
+    return this;
 };
 
-MochaSauce.prototype.browser = function(conf) {
-	debug('add %s %s %s', conf.browserName || conf.app, conf.version, conf.platform);
-	conf.version = conf.version || '';
-	this.browsers.push(conf);
+MochaSauce.prototype.url = function (url) {
+    this._url = url;
+    return this;
 };
 
-MochaSauce.prototype.start = function(fn) {
+MochaSauce.prototype.concurrency = function (num) {
+    this._concurrency = num;
+    return this;
+};
 
-	var self = this;
-	var batch = new Batch();
-	fn = fn || function() {};
+MochaSauce.prototype.record = function (video, screenshots) {
 
-	batch.concurrency(this._concurrency);
+    if (screenshots === undefined) {
+        screenshots = video;
+    }
 
-	this.browsers.forEach(function(conf) {
-		conf.tags = self.tags;
-		conf.name = self.name;
-		conf.build = self.build;
+    this._video = video;
+    this._screenshots = screenshots;
 
-		// disable Sauce features not needed for unit tests (video + screenshot recording)
-		conf['record-video'] = self._video;
-		conf['record-screenshots'] = self._screenshots;
+    return this;
+};
 
-		batch.push(function(done) {
+MochaSauce.prototype.browser = function (conf) {
+    debug('add %s %s %s', conf.browserName || conf.app, conf.version, conf.platform);
+    conf.version = conf.version || '';
+    this.browsers.push(conf);
+};
 
-			// initialize remote connection to Sauce Labs
-			debug('running %s %s %s', conf.browserName || conf.app, conf.version, conf.platform);
-			var browser = wd.remote(self.host, self.port, self.user, self.key);
-			self.emit('init', conf);
+MochaSauce.prototype.start = function (test, fn) {
 
-			browser.init(conf, function() {
+    var self = this;
+    var batch = new Batch();
+    fn = fn || function () {};
 
-                self._urls.forEach( function (url) {
-                    debug('open %s', url);
-                    self.emit('start', conf);
+    batch.concurrency(this._concurrency);
 
-                    // load the test site
-                    browser.get(url, function(err) {
-                        if (err) return done(err);
+    this.browsers.forEach(function (conf) {
+        conf.tags = self.tags;
+        conf.name = self.name;
+        conf.build = self.build;
 
-                        // wait until choco is ready
-                        function doItAgain() {
+        // disable Sauce features not needed for unit tests (video + screenshot recording)
+        conf['record-video'] = self._video;
+        conf['record-screenshots'] = self._screenshots;
 
-                            browser.eval('window.chocoReady', function(err, res) {
+        batch.push(function (done) {
 
-                                if(res !== true) {
-                                    setTimeout(function() {
-                                        doItAgain();
-                                    }, 1000);
-                                    return;
-                                }
+            // initialize remote connection to Sauce Labs
+            debug('running %s %s %s', conf.browserName || conf.app, conf.version, conf.platform);
+            var browser = wd.remote(self.host, self.port, self.user, self.key);
+            self.emit('init', conf);
 
+            browser.init(conf, function () {
+
+                debug('open %s', self._url);
+                self.emit('start', conf);
+
+                // load the test site
+                browser.get(self._url, function (err) {
+                    if (err) return done(err);
+
+                    var testsRan = false;
+                    // wait until choco is ready
+
+                    function doItAgain() {
+
+                        browser.eval('window.chocoReady', function (err, res) {
+
+                            if (res !== true) {
+                                setTimeout(function () {
+                                    doItAgain();
+                                }, 1000);
+                                return;
+                            }
+
+                            if (err) return done(err);
+
+                            browser.eval('JSON.stringify(window.mochaResults)', function (err, res) {
                                 if (err) return done(err);
 
-                                browser.eval('JSON.stringify(window.mochaResults)', function(err, res) {
-                                    if (err) return done(err);
+                                // convert stringified object back to parsed
+                                res = JSON.parse(res);
 
-                                    // convert stringified object back to parsed
-                                    res = JSON.parse(res);
+                                // add browser conf to be able to identify in the end callback
+                                res.browser = conf;
 
-                                    // add browser conf to be able to identify in the end callback
-                                    res.browser = conf;
+                                debug('results %j', res);
 
-                                    debug('results %j', res);
+                                // update Sauce Labs with custom test data
+                                var data = {
+                                    'custom-data': {
+                                        mocha: res.jsonReport
+                                    },
+                                    'passed': !res.failures
+                                };
 
-                                    // update Sauce Labs with custom test data
-                                    var data = {
-                                        'custom-data': { mocha: res.jsonReport },
-                                        'passed': !res.failures
-                                    };
+                                request({
+                                    method: "PUT",
+                                    uri: ["https://", self.user, ":", self.key, "@saucelabs.com/rest", "/v1/", self.user, "/jobs/", browser.sessionID].join(''),
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    },
+                                    body: JSON.stringify(data)
+                                }, function ( /*error, response, body*/ ) {
 
-                                    request({
-                                        method: "PUT",
-                                        uri: ["https://", self.user, ":", self.key, "@saucelabs.com/rest", "/v1/", self.user, "/jobs/", browser.sessionID].join(''),
-                                        headers: {'Content-Type': 'application/json'},
-                                        body: JSON.stringify(data)
-                                    }, function (/*error, response, body*/) {
-
-                                        self.emit('end', conf, res);
-                                        browser.quit();
-                                        done(null, res);
-
+                                    self.emit('end', conf, res);
+                                    browser.quit(function (err) {
+                                        self.emit('quit', conf);
                                     });
-
+                                    done(null, res);
                                 });
 
                             });
-                        }
 
-                        doItAgain();
+                        });
+                    }
 
-                    });
+                    if (!testsRan && test && typeof test === 'function') {
+                        test(browser, function (err) {
+                            testsRan = true;
+                            if (typeof err !== 'undefined' && err !== null) {
+                                self.emit('error', conf);
+                                done(err);
+                            } else {
+                                doItAgain();
+                            }
+                            //doItAgain.call(this);
+                        });
+                    } else {
+                        doItAgain()
+                    }
 
-                })
-			});
-		});
-	});
+                });
+            });
+        });
+    });
 
-	batch.end(fn);
+    batch.end(fn);
 
 };
 
